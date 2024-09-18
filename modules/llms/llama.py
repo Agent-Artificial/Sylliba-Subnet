399cc8c5e3b867cacada236779b91026a3ec2906
mltrev23 <trevor.dev23@gmail.com>
2024-09-18 12:06:51 +0000

    feat: llama to translate only

--- modules/llms/llama.py
+++ modules/llms/llama.py
@@ -2,7 +2,7 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 import torch
 import json
 
-def process(messages, source_language, target_language):
+def process(messages):
     model_id = "meta-llama/Meta-Llama-3.1-8B-Instruct"
     
     quant_config = BitsAndBytesConfig(
@@ -29,22 +29,30 @@ def process(messages, source_language, target_language):
     )
 
     response = get_pipeline(messages, max_length = 1000)
-    text = response[0]['generated_text'][1]['content']
-    print(f'text: {text}')
-    content = json.loads(text)
-    print(f'content: {content}')
-    # input_data = text.split(f"{source_language}:")[1].split(f"{target_language}:")[0]
-    # output_data = text.split(f"{target_language}:")[1]
-    input_data, output_data = content[source_language], content[target_language]
+    text = response[0]['generated_text'][-1]['content']
+    return text
 
-    return input_data, output_data
-
-if __name__ == 'main':
-    messages = """Sylliba is a revolutionary translation module designed to bridge the gap in communication across diverse languages. With the capability to translate many languages, Sylliba supports both audio and text for inputs and outputs, making it a versatile tool for global interactions.
+if __name__ == '__main__':
+    text = """Sylliba is a revolutionary translation module designed to bridge the gap in communication across diverse languages. With the capability to translate many languages, Sylliba supports both audio and text for inputs and outputs, making it a versatile tool for global interactions.
 As our first step into the Bittensor ecosystem, Sylliba connects to the network we are building, providing AI tooling and linking various blockchain networks together. Our mission is to create a seamless and intuitive translation experience that leverages advanced AI to foster better understanding and collaboration across different languages and cultures.
-Explore Sylliba and experience the future of translation here"""
+Explore Sylliba and experience the future of translation here."""
     source_language = "English"
     target_language = "French"
-    input_data, output_data = process(messages, source_language, target_language)
-    print(f"input_data: {input_data}")
+
+    messages = [
+        {
+            "role": "system",
+            "content": f"""
+            Provided text is written in {source_language}.
+            Please translate into {target_language}
+            Don't put any tags, description or decorators.
+            Write only translated text in raw text format.
+            """
+        }, 
+        {
+            "role": "user",
+            "content": text
+        }
+    ]
+    output_data = process(messages)
     print(f"output_data: {output_data}")
