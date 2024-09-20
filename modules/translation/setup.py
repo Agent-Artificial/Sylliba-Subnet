25346c4948246f83f6404d818b1c024054f2a0ce
mltrev23 <trevor.dev23@gmail.com>
2024-09-19 19:07:29 -0500

    feat: removed unused files

--- modules/translation/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="translation_module",
-    version="1",
-    entry_points={
-        "translation": ["translation=modules.translation.translation.Translation:process"],
-    }
-)
