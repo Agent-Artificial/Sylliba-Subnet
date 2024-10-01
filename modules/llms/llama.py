2b0f44af73ec6f83430e2d9411bfa6b2dd249877
mltrev23 <trevor.dev23@gmail.com>
2024-10-01 21:06:04 +0000

    feat: update llama from unslothai

--- modules/llms/llama.py
+++ modules/llms/llama.py
@@ -16,10 +16,16 @@ def process(messages: List[Dict[str, Any]], device = torch.device("cuda" if torc
     """
     if 'llama' not in MODELS:
         MODELS['llama'] = load_llama(device)
-    llm = MODELS['llama']
+    model, tokenizer = MODELS['llama']
     
-    result = llm.create_chat_completion(messages = messages)
-    return result['choices'][0]['message']['content']
+    input_data = '\n'.join([entry['content'] for entry in messages])
+    inputs = tokenizer(input_data, return_tensors = "pt").to("cuda")
+
+    outputs = model.generate(**inputs, max_new_tokens = 1000, use_cache = True)
+    new_tokens = outputs[:, inputs['input_ids'].shape[-1]:]
+    result = tokenizer.batch_decode(new_tokens, skip_special_tokens=True)
+
+    return result[0]
 
 if __name__ == '__main__':
     text = """Sylliba is a revolutionary translation module designed to bridge the gap in communication across diverse languages. With the capability to translate many languages, Sylliba supports both audio and text for inputs and outputs, making it a versatile tool for global interactions.
--- neurons/utils/model_load.py
+++ neurons/utils/model_load.py
@@ -1,5 +1,5 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig, pipeline, T5Tokenizer, T5ForConditionalGeneration, AutoProcessor, SeamlessM4Tv2Model
-from llama_cpp import Llama
+from unsloth import FastLanguageModel
 import torch
 
 def load_flan_t5_large(device = torch.device("cuda" if torch.cuda.is_available() else "cpu")):
@@ -42,9 +42,16 @@ def load_seamless(device = torch.device("cuda" if torch.cuda.is_available() else
     return model, processor
 
 def load_llama(device = torch.device("cuda" if torch.cuda.is_available() else "cpu")):
-    llm = Llama.from_pretrained(
-        repo_id="bartowski/Meta-Llama-3.1-8B-Instruct-GGUF",
-        filename="Meta-Llama-3.1-8B-Instruct-IQ2_M.gguf",
-    )
+    max_seq_length = 2048 # Choose any! We auto support RoPE Scaling internally!
+    dtype = None # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
+    load_in_4bit = True # Use 4bit quantization to reduce memory usage. Can be False.
 
-    return llm
+    model, tokenizer = FastLanguageModel.from_pretrained(
+        model_name = "unsloth/llama-3-8b-Instruct-bnb-4bit", # Choose ANY! eg teknium/OpenHermes-2.5-Mistral-7B
+        max_seq_length = max_seq_length,
+        dtype = dtype,
+        load_in_4bit = load_in_4bit,
+    )
+    FastLanguageModel.for_inference(model) 
+
+    return model, tokenizer
