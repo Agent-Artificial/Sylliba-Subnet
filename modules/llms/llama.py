4725ac2b8e984a03776da61f6b4ec1fb0505d700
mltrev23 <trevor.dev23@gmail.com>
2024-10-02 01:03:08 +0000

    feat: dolphin llama

--- modules/llms/llama.py
+++ modules/llms/llama.py
@@ -17,15 +17,26 @@ def process(messages: List[Dict[str, Any]], device = torch.device("cuda" if torc
     if 'llama' not in MODELS:
         MODELS['llama'] = load_llama(device)
     model, tokenizer = MODELS['llama']
-    
-    input_data = '\n'.join([entry['content'] for entry in messages])
-    inputs = tokenizer(input_data, return_tensors = "pt").to("cuda")
 
-    outputs = model.generate(**inputs, max_new_tokens = 1000, use_cache = True)
-    new_tokens = outputs[:, inputs['input_ids'].shape[-1]:]
-    result = tokenizer.batch_decode(new_tokens, skip_special_tokens=True)
+    def preprocess(messages):
+        text = [f"<|im_start|>{message['role']}\n{message['content']}<|im_end|>" for message in messages]
+        text = "\n".join(text)
+        return text.strip() + '<|im_start|>assistant'
 
-    return result[0]
+    messages = preprocess(messages)
+
+    # Prepare the input question
+    input_ids = tokenizer.encode(messages, return_tensors="pt").to(device)
+
+    # Generate answer
+    with torch.no_grad():
+        output_ids = model.generate(input_ids, max_length=400, num_return_sequences = 1).to(device)
+
+    # Decode the generated answer
+    output_answer = tokenizer.decode(output_ids[0], skip_special_tokens=True)
+    output_answer = output_answer.split("<|im_start|>assistant")[1].strip()
+
+    return output_answer
 
 if __name__ == '__main__':
     text = """Sylliba is a revolutionary translation module designed to bridge the gap in communication across diverse languages. With the capability to translate many languages, Sylliba supports both audio and text for inputs and outputs, making it a versatile tool for global interactions.
--- neurons/utils/model_load.py
+++ neurons/utils/model_load.py
@@ -42,16 +42,22 @@ def load_seamless(device = torch.device("cuda" if torch.cuda.is_available() else
     return model, processor
 
 def load_llama(device = torch.device("cuda" if torch.cuda.is_available() else "cpu")):
-    max_seq_length = 2048 # Choose any! We auto support RoPE Scaling internally!
-    dtype = None # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
-    load_in_4bit = True # Use 4bit quantization to reduce memory usage. Can be False.
+    
+    model_id = "cognitivecomputations/dolphin-2.9.4-llama3.1-8b"
 
-    model, tokenizer = FastLanguageModel.from_pretrained(
-        model_name = "unsloth/llama-3-8b-Instruct-bnb-4bit", # Choose ANY! eg teknium/OpenHermes-2.5-Mistral-7B
-        max_seq_length = max_seq_length,
-        dtype = dtype,
-        load_in_4bit = load_in_4bit,
+    quant_config = BitsAndBytesConfig(
+        load_in_4bit=True,           # This flag is now part of BitsAndBytesConfig
+        bnb_4bit_use_double_quant=True,  # Optional, for double quantization
+        bnb_4bit_quant_type="nf4",   # Choose between 'fp4' or 'nf4' (Non-negative quantization)
     )
-    FastLanguageModel.for_inference(model) 
+
+    if not hasattr(AutoModelForCausalLM, 'cached_model'):
+        AutoModelForCausalLM.cached_model = AutoModelForCausalLM.from_pretrained(
+            model_id,
+            quantization_config=quant_config,  # 4-bit Quantization config
+            torch_dtype=torch.bfloat16,        # Mixed precision (optional, use bfloat16 for efficiency)
+        ).to(device)
+    model = AutoModelForCausalLM.cached_model
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
 
     return model, tokenizer
