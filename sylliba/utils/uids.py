f0354be11f7fa45829f9fd4e91c91118fd08bfe8
mltrev23 <trevor.dev23@gmail.com>
2024-10-16 12:14:12 +0000

    feat: add note

--- sylliba/utils/uids.py
+++ sylliba/utils/uids.py
@@ -2,6 +2,7 @@ import random
 import bittensor as bt
 import numpy as np
 from typing import List
+from sylliba.api.get_query_axons import ping_uids
 
 
 def check_uid_availability(
@@ -28,6 +29,53 @@ def check_uid_availability(
     # Available otherwise.
     bt.logging.debug(f"\tavailable:succeeded availability check")
     return True
+
+async def get_top_miner_uids(metagraph: "bt.metagraph.Metagraph",
+                             wallet: "bt.wallet.Wallet",
+                             top_rate: float = 1,
+                             vpermit_tao_limit: int = 4096) -> np.int64:
+
+    """Returns the available top miner UID from the metagraph.
+    Args:
+        metagraph (bt.metagraph.Metagraph): Metagraph object
+        dendrite (bt.dendrite.Dendrite): Dendrite object
+        top_rate (float): The fraction of top nodes to consider based on stake. Defaults to 1.
+        vpermit_tao_limit (int): Validator permit tao limit
+        exclude (List[int]): List of uids to exclude from the random sampling.
+    Returns:
+        top_miner_uid (np.int64): The top miner UID.
+    """
+    try:
+        dendrite = bt.dendrite(wallet=wallet)
+        miner_candidate_uids = []
+        for uid in range(metagraph.n.item()):
+            uid_is_available = check_uid_availability(
+                metagraph, uid, vpermit_tao_limit
+            )
+
+            if uid_is_available:
+                miner_candidate_uids.append(uid)
+
+        miner_healthy_uids, _ = await ping_uids(dendrite, metagraph, miner_candidate_uids)
+
+        ips = []
+        miner_ip_filtered_uids = []
+        for uid in miner_healthy_uids:
+            if metagraph.axons[uid].ip not in ips:
+                ips.append(metagraph.axons[uid].ip)
+                miner_ip_filtered_uids.append(uid)
+
+        # Consider both of incentive and trust score
+        values = [(uid, metagraph.I[uid] * metagraph.trust[uid]) for uid in miner_ip_filtered_uids]
+        sorted_values = sorted(values, key=lambda x: x[1], reverse=True)
+        top_rate_num_items = max(1, int(top_rate * len(miner_ip_filtered_uids)))
+        top_miner_uids = np.array([uid for uid, _ in sorted_values[:top_rate_num_items]])
+        return top_miner_uids
+    except Exception as e:
+        bt.logging.error(f"Failed to get top miner uids", error = {'exception_type': e.__class__.__name__,'exception_message': str(e),'exception_args': e.args})
+        return None
+    finally:
+        dendrite.close_session()
 
 
 def get_miner_uids(
