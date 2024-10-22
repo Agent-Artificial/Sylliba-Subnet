c4326588adf6c2c312b64d0ad76fa5c0310473df
mltrev23 <trevor.dev23@gmail.com>
2024-10-22 18:24:31 +0000

    feat: db manager

---
+++ db/db_manager.py
@@ -0,0 +1,34 @@
+from sqlalchemy import Column, Integer, String, Float, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from neurons.utils.get_pg_url import get_postgres_url
+
+Base = declarative_base()
+
+class PromptsTable(Base):
+    __tablename__ = 'promptstable'
+    miner_uid = Column(Integer)
+    input_data = Column(String)
+    task_type = Column(String)
+    source_language = Column(String)
+    target_langauge = Column(String)
+    miner_response = Column(String)
+    text_score = Column(Float)
+    audio_quality_score = Column(Float)
+    overall_score = Column(Float)
+
+class DBManager:
+    def __init__(self, url = get_postgres_url()) -> None:
+        self.engine = create_engine(url)
+        self.Session = sessionmaker(bind = self.engine)
+
+        Base.metadata.create_all(self.engine)
+    
+    def add_entry(self, miner_uid, input_data, task_type, source_language, target_language, miner_response, text_score, audio_quality_score, overall_score):
+        new_entry = PromptsTable(miner_uid = miner_uid, input_data = input_data, task_type = task_type, source_language = source_language,
+                                 target_language = target_language, miner_response = miner_response, text_score = text_score, 
+                                 audio_quality_score = audio_quality_score, overall_score = overall_score)
+        with self.Session() as session:
+            session.add(new_entry)
+            session.commit()
